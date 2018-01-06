8694799c0a4306d39fdb2cac70f0d3e03097358f
Jon Jackson <Jax24135@yahoo.com>
2018-01-06 00:01:56 -0600

    working on getting TAGLESS version
    - think it's a unicode "music symbol" issue, trying to skip over invalid symbols
    - also need to start overwriting old versions or incrementing new ones.. this seek & destroy is getting old
    - figure out how to distinuging between 'file' as in fileNAME and file as in readable text file..may have to do this in another script

--- SRTextractor.py
+++ SRTextractor.py
@@ -1,26 +1,47 @@
 #!/usr/bin/env python
+import ffmpy, os, fileinput, codecs
 
+# GLOBAL CONSTANTS
+CHAR_CUT = -4           # how many character to cut off (i.e. ".mp4" is -4
+ARGS = ['-map','0:s:0' ] # arguments/options to implement when creating new OUTPUT file
+ADD_TO_NAME = ".en.srt" # after .mp4 is removed, add this to filename
 
-import ffmpy, os, subprocess
-
-CHAR_CUT = -4   # how many character to cut off (i.e. ".mp4" is -4
-ARGS = ['-map','0:s:0']
-
+# Implements FFMPEG arguments on $targetFile
 def extractMe(file):
-    ff = ffmpy.FFmpeg(  # calls FFMPEG to perform operation
-            inputs={file: None},  # call original INPUT $file
-            outputs={newName(file):ARGS}
+    ff = ffmpy.FFmpeg(                      # calls FFMPEG from ffmpy module
+            inputs = { file : None},            # call original INPUT $file
+            outputs = { newName(file) : ARGS}    # adjust name and implement arguments/options
     )
-    ff.run()
+    ff.run()                                # creates OUTPUT file
 
+# Splice .mp4 out of name during new file creation
 def newName(file):
-    newA = file[:CHAR_CUT]
-    return newA+".en.srt"
+    cutName = file[:-4]       # make a new string based on .mp4 being cut
+    return cutName+ADD_TO_NAME    # add whatever the User wants to filename
 
 def main():
-    for file in os.listdir():  # for every file in current directory
-        if file.endswith('mp4'):  # if the file is an .mp4
-            extractMe(file)  # save the mp4's caption as an .srt
 
+    for file in os.listdir():       # for every file in current directory
+        if file.endswith('mp4'):    # if the file is an .mp4
+            extractMe(file)         # save the mp4's caption as an .srt.. or whatever other arguments User wants (see top)
+
+    myOut = open(file+"NEW.rtf",'w')
+
+    with codecs.open(os.listdir(),"r",encoding="utf-8",errors="ignore"):
+
+        for line in file:
+                myOut.write(line)
+
+
+
+
+    #myIn.close()
+    myOut.close()
+
+    #with fileinput.FileInput(file,inplace=True,backup=".bak") as file:
+    #    for line in file:
+    #        line.replace('<font size="24">' , '' , end='')
+    #        line.replace('</font>' , '' , end='')
+    #        line.replace('<font face="Arial">' , '', end='')
 
 main()
