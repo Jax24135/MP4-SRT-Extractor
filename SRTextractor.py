f1032386e99df134aea51b42e1e7f7850ded9a5e
Jon Jackson <Jax24135@yahoo.com>
2018-01-06 22:20:17 -0600

    everything works
    SRT extracted, renamed correctly, HTML tags removed + extra tmp files deleted
    .. needs more comments though

--- SRTextractor.py
+++ SRTextractor.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 import ffmpy, os, fileinput, codecs
+from shutil import copyfile
 
 # GLOBAL CONSTANTS
-CHAR_CUT = -4           # how many character to cut off (i.e. ".mp4" is -4
-ARGS = ['-map','0:s:0' ] # arguments/options to implement when creating new OUTPUT file
-ADD_TO_NAME = ".en.srt" # after .mp4 is removed, add this to filename
+DIR = '.'                 # target directory to run through
+CHAR_CUT = -4             # how many character to cut off (i.e. ".mp4" is -4
+ARGS = ['-map','0:s:0' ]  # arguments/options to implement when creating new OUTPUT file
+ADD_TO_NAME = ".en.srt"   # after .mp4 is removed, add this to filename
 
 # Implements FFMPEG arguments on $targetFile
-def extractMe(file):
+def run_ffmpeg_on_file(file):
     ff = ffmpy.FFmpeg(                      # calls FFMPEG from ffmpy module
             inputs = { file : None},            # call original INPUT $file
             outputs = { newName(file) : ARGS}    # adjust name and implement arguments/options
@@ -21,36 +23,28 @@ def newName(file):
 
 def main():
 
+    # run FFMPEG on all files in pwd
     for file in os.listdir():       # for every file in current directory
         if file.endswith('mp4'):    # if the file is an .mp4
-            extractMe(file)         # save the mp4's caption as an .srt.. or whatever other arguments User wants (see top)
+            run_ffmpeg_on_file(file)         # save the mp4's caption as an .srt.. or whatever other arguments User wants (see top)
 
-    #print("before change " + file)
+    #re-align filename with
     file = newName(file)
-    #print("post change " + file)
 
     myIn = open(file,'r')
     myOut = open(file+"NEW.rtf",'w')
 
-    #with codecs.open(os.listdir(),"r",encoding="utf-8",errors="ignore"):
-
     for line in myIn:
-        line.replace('<font size="24">','')
-        #line.replace('</font>' , '' , end='')
-        #line.replace('<font face="Arial">' , '', end='')
-
+        line = line.replace('<font size="24">','')
+        line = line.replace('</font>' , '' )
+        line = line.replace('<font face="Arial">' , '')
         myOut.write(line)
-
-
-
 
     myIn.close()
     myOut.close()
 
-    #with fileinput.FileInput(file,inplace=True,backup=".bak") as file:
-    #    for line in file:
-    #        line.replace('<font size="24">' , '' , end='')
-    #        line.replace('</font>' , '' , end='')
-    #        line.replace('<font face="Arial">' , '', end='')
+    os.remove(file)
+    copyfile(file+"NEW.rtf",file)
+    os.remove(file+"NEW.rtf")
 
 main()
