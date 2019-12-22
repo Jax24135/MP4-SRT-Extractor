bd590e494a153b9db021482760117e69426de701
Jon Jackson <Jax24135@yahoo.com>
2019-12-21 21:30:03 -0600

    Create srt_extractor_v2.py
    Pull from GitLab to private repo

---
+++ srt_extractor_v2.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+#JonMSG - finally removed pesky <HTML_TAGS>, is there a cleaner way to run through REPLACE list?
+
+import ffmpy, os
+#import os
+#from ffmpy import FFmpeg
+#from os import path
+from shutil import copyfile
+
+# GLOBAL CONSTANTS
+DIR = '.'                 # target directory to run through
+CHAR_CUT = -4             # how many character to cut off (i.e. ".mp4" is -4)
+ARGS = ['-map','0:s:0','-loglevel','panic' ]  # arguments/options to implement when creating new OUTPUT file
+ADD_TO_NAME = ".en.srt"   # after .mp4 is removed, add $this to filename
+
+# remove these tags from each line in SRT file
+HTML_TAGS = ['<font size="24">', \
+             '\r<font size="24">', \
+             '</font>', \
+             '</font>\r', \
+             '<font face="Arial">', \
+             '<font face="Arial">\r', \
+             'CAPTIONING MADE POSSIBLE BY', \
+             '    WARNER BROS.', \
+             'CAPTIONED BY THE NATIONAL', \
+             'CAPTIONING INSTITUTE', \
+             '--www.ncicap.org--']
+
+def main():
+
+    # run FFMPEG on all files in pwd - IF file ends in ".mp4"
+    for file in os.listdir(DIR):                        # for every file in current directory
+        if file.endswith('mp4'):                        # if the file is an .mp4.. do below
+            newFile = file[:CHAR_CUT] + ADD_TO_NAME           # remove 'mp4 and add '.en.srt' to filename
+            
+            if os.path.isfile(newFile):                 # if the '.en.srt' file already exists
+                os.remove(newFile)                      # delete previous SRT file
+                
+            ff = ffmpy.FFmpeg (
+                inputs = { file : None },               # call original INPUT file
+                outputs = { newFile : ARGS })           # save file as newFile and use global ARGuments
+            
+            ff.run()                                     # create the extracted SRT file
+            
+            #if file.is_dir():                          # JonMSG - not sure what this is...
+            #   os.remove(file)
+
+
+            
+            myIn = open(newFile , 'r')
+            myOut = open(newFile +".tmp.rtf" , 'w')
+
+            for line in myIn:
+                for tag in HTML_TAGS:
+                    line = line.replace(tag,'')
+                myOut.write(line)
+
+            myIn.close()
+            myOut.close()
+
+            #  folder cleanup
+
+            #os.remove(file)                        # JonMSG - why delete the original file? (in case a VOB.tmp file)
+            copyfile(newFile+".tmp.rtf",newFile)       # copy new file name into original filename spot
+            os.remove(newFile+".tmp.rtf")           # remove file name with "NEW" tag  #decluttering
+
+
+print('\n'+"finished w/o errors")  # confirm clean exit
+
+main()
